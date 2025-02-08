b810f976aa528eccefb28ffeea609273bbf953fe
Arpit08-coder-glitch <arpitsin28@gmail.com>
2025-02-08 17:57:13 +0530

    TailWind Warning Ignored

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Adjust if necessary
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Ensure correct paths
   theme: {
     extend: {},
   },
